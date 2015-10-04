9a688f58e29188a8167a8972c0d3d7c2cfc565ae
Mark Haskamp <mark.haskamp@pinnsg.com>
2015-10-04 13:37:32 -0400

    using state

--- app/view/press_count.js
+++ app/view/press_count.js
@@ -1,19 +1,26 @@
 
 var PressCount = React.createClass({
   getInitialState: function() {
-    return {pressCount: 42};
+    return {pressCount: 0};
   },
 
   render: function() {
     return (
       <div>Pressed {this.state.pressCount} times
-        <div><button onClick={this.handleClick}>Click me</button></div>
+        <div>
+          <button onClick={this.handleClick}>+1</button>
+          <button onClick={this.handleReset}>reset</button>
+        </div>
       </div>
     )
   },
 
   handleClick: function() {
     this.setState({pressCount: this.state.pressCount + 1});
+  },
+
+  handleReset: function() {
+    this.setState({pressCount: 0});
   }
 
 });
