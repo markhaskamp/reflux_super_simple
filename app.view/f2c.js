83433858e324e018c14bf790017b23168fe3e623
Mark Haskamp <mark.haskamp@pinnsg.com>
2015-10-04 12:38:46 -0400

    wip

---
+++ app.view/f2c.js
@@ -0,0 +1,14 @@
+
+var F2C = React.createClass({
+  render: function() {
+    return (
+      <p>Hello, Universe</p>
+    )
+  }
+
+});
+
+React.render(
+    <F2C/>, document.getElementById('f2c')
+);
+
